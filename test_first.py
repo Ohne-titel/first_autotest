1a1df81a54f1f3e23a09dd754a67253a96591c64
Ohne-titel <kisterlina@gmail.com>
2023-11-11 11:56:00 +0300

    добавлен позитивный тест в test_first.py

--- test_first.py
+++ test_first.py
@@ -2,6 +2,12 @@ from selene.support.shared import browser
 from selene import be, have
 
 
+def test_positive_chrome(browser_configs):
+    browser.open('https://google.com')
+    browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
+    browser.element('[id="search"]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
+
+
 def test_open_chrome(browser_configs):
     browser.element('[class="gLFyf"]').click().type('shmekelbesh7mene5').press_enter()
     browser.element('[style="padding-top:.33em"]').should(have.text('Es wurden keine mit deiner Suchanfrage - '
